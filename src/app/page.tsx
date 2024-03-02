1093375348c34889bea66cbc3f9385aae6a28be1
shivamvora <vorashivam0@gmail.com>
2024-03-02 12:25:06 +0530

    build error solve

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,26 @@
-import { Category } from '@/components/category'
-import { Footer } from '@/components/footer'
-import { HeroSection } from '@/components/herosection'
-import { HotDeals } from '@/components/hotdeals'
-import { Navbar } from '@/components/navbar'
+import { Category } from "@/components/category";
+import { Footer } from "@/components/footer";
+import { HeroSection } from "@/components/herosection";
+import { HotDeals } from "@/components/hotdeals";
+import { Navbar } from "@/components/navbar";
 // import SliderBg from '../../public/assets/images/slider_bg.jpg'
 
 export default function Home() {
   return (
     <>
       <div className="hero-background">
-        <div className='container mx-auto'>
+        <div className="container mx-auto">
           <Navbar />
           <HeroSection />
-          <div className='bg-white rounded-xl'>
+          <div className="bg-white rounded-xl">
             <div className="container mx-auto">
               <Category />
-              <HotDeals/>
+              <HotDeals />
             </div>
             <Footer />
           </div>
         </div>
       </div>
     </>
-  )
+  );
 }
